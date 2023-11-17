326d9a43b8f8a4b71e5428b67d5286e29767cdf0
BaAniq <basia.anisimowicz@gmail.com>
2023-11-17 15:31:25 +0100

    Adding a calculate button and a function

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from tkinter import *
 
+def calculate():
+    value_mile = int(entry_box.get())
+    value_km = value_mile * 1.60934
+    result.config(text=f'{value_km}')
 
 window = Tk()
 window.config(padx=10, pady=10)
-window.minsize(width=300, height=200)
+window.minsize(width=300, height=100)
 window.title('Mile to Km Converter')
 
 entry_box = Entry(width=15)
@@ -21,6 +25,9 @@ result.grid(column=2, row=2)
 
 label_km = Label(text='Km')
 label_km.grid(column=3, row=2)
+
+calculate_button = Button(text='Calculate', command=calculate)
+calculate_button.grid(column=2, row=3)
 
 
 
